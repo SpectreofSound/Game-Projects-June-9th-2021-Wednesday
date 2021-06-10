0235133498f40d9c053cd2907591f3580ddb43b2
SpectreofSound <thesketchartist97@gmail.com>
2021-06-10 13:41:22 -0700

    Initial commit

---
+++ Mad-Libs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color:")
+adjective = input("Enter a adjective:")
+noun = input("Enter a noun:")
+
+print("Summer is " + adjective)
+print("The trees are " + color)
+print("You can ride on a " + noun)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Game-Projects-June-9th-2021-Wednesday
+ Small projects such as Games will created using HTML, CSS, a little Python
